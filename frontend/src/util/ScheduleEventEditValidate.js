ea2b2f772c74635ddccc370e212c012e09e1c85a
Ted Weatherly <ted.weatherly@gmail.com>
2017-01-03 06:05:35 -0600

    Add validation that start date is before end date

--- frontend/src/util/ScheduleEventEditValidate.js
+++ frontend/src/util/ScheduleEventEditValidate.js
@@ -7,7 +7,7 @@ validate.extend(validate.validators.datetime, {
 });
 
 export default (values) => {
-	const results = validate(values, {
+	const errors = validate(values, {
 		title: {
 			presence: true, // required
 			length: { // must be between 5 and 20 characters long
@@ -36,7 +36,7 @@ export default (values) => {
 			format: {
 				pattern: /^[a-zA-Z][a-zA-Z0-9\.-_ \"']+$/, // only allow safe characters, must start with a letter
 				message: "must start with a letter and can only contain safe characters"
-			},
+			}
 		},
 		description: {
 			format: {
@@ -45,12 +45,18 @@ export default (values) => {
 			}
 		}
 	});
+	if (values.start_dt >= values.end_dt) {
+		if (errors.start_dt == null) errors.start_dt = [];
+		errors.start_dt.push("Start Date/Time must occur before the Start Date/Time");
+		if (errors.end_dt == null) errors.end_dt = [];
+		errors.end_dt.push("End Date/Time must occur after the Start Date/Time");
+	}
 	// only show one error at a time
-	for (let key in results) {
-		if (results.hasOwnProperty(key)) {
-			const val = results[key];
-			if (Array.isArray(val)) results[key] = val[0];
+	for (let key in errors) {
+		if (errors.hasOwnProperty(key)) {
+			const val = errors[key];
+			if (Array.isArray(val)) errors[key] = val[0];
 		}
 	}
-	return results;
+	return errors;
 };
