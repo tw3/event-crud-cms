94123e259fe801664c346af6af8f7613d99ada57
Ted Weatherly <ted.weatherly@gmail.com>
2017-01-03 18:38:55 -0600

    Reset form when navigating from Edit Event to Add Event

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -6,6 +6,16 @@ import scheduledEvents from "./scheduledEvents";
 // main reducers
 export const reducers = combineReducers({
   routing: routerReducer,
-  form: formReducer,
+  form: formReducer.plugin({
+		"event-edit": (state, action) => {
+			// reset form (wipe state) when navigating away from the Edit Event page
+			switch(action.type) {
+				case "@@router/LOCATION_CHANGE":
+					return undefined;
+				default:
+					return state;
+			}
+		}
+	}),
   scheduledEvents: scheduledEvents
 });
